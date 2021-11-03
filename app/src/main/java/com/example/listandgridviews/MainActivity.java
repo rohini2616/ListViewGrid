40cc61547f944fc4e553ec99bd10d22040d33215
Rohini <rohinisrinivasan97@gmail.com>
2021-11-03 10:11:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/listandgridviews/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.listandgridviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+ private Button buttonList;
+ private Button buttonGrid;
+ private ListView listView;
+ private GridView gridView;
+ String[] names={"Dhoni","Raina","Rohit","Ashwin","Saāchin"};
+ int[] images={R.drawable.dhoni,R.drawable.raina,
+         R.drawable.rohit,R.drawable.ashwin,R.drawable.sachin};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonList=findViewById(R.id.btnList);
+        buttonGrid=findViewById(R.id.btnGrid);
+        listView=findViewById(R.id.lvCollection);
+        gridView=findViewById(R.id.gvCollection);
+
+
+        buttonGrid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonGrid.setBackgroundColor(getResources().getColor(R.color.colorGreen));
+                buttonList.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+                PlayersAdapter adapter = new PlayersAdapter(MainActivity.this,names,images,"grid");
+                gridView.setAdapter(adapter);
+                listView.setVisibility(View.GONE);
+                gridView.setVisibility(View.VISIBLE);
+            }
+        });
+        buttonList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonList.setBackgroundColor(getResources().getColor(R.color.colorGreen));
+                buttonGrid.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+                PlayersAdapter adapter = new PlayersAdapter(MainActivity.this,names,images,"list");
+                listView.setAdapter(adapter);
+                listView.setVisibility(View.VISIBLE);
+                gridView.setVisibility(View.GONE);
+            }
+        });
+    }
+}
